6806bd0d9fe94d566efc8a7045571927a9997936
danadrol96 <danpark105@gmail.com>
2021-09-26 03:40:06 -0400

    refactor so navbar doesnt show up on login page

--- client/src/App.js
+++ client/src/App.js
@@ -2,21 +2,41 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import { GlobalStyle } from "./globalStyles";
 import Home from "./pages/Home";
+import Login from "./pages/Login";
 import Profile from "./pages/Profile";
+
+const LoginContainer = () => (
+  <>
+    <Route exact path="/login">
+      <Login />
+    </Route>
+  </>
+);
+
+const DefaultContainer = () => (
+  <>
+    <Navbar />
+
+    <Route exact path="/">
+      <Home />
+    </Route>
+    <Route exact path="/login">
+      <Login />
+    </Route>
+    <Route exact path="/:username">
+      <Profile />
+    </Route>
+  </>
+);
 
 function App() {
   return (
     <>
       <GlobalStyle />
       <Router>
-        <Navbar />
         <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route path="/:username">
-            <Profile />
-          </Route>
+          <Route exact path="/login" component={LoginContainer} />
+          <Route component={DefaultContainer} />
         </Switch>
       </Router>
     </>
---
+++ client/src/pages/Login/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  width: 100%;
+  display: flex;
+`;
+const Login = () => {
+  return <Container>Login</Container>;
+};
+
+export default Login;
